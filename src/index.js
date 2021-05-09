6b5e4f69410c40287fe3a34670c89c4d1ea08d0a
Jay_WooKJ <natcho9010@gmail.com>
2021-05-09 14:52:35 +0900

    Add: deadZone Config

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ const WIDTH = 1152;
 const HEIGHT = 647;
 const START_POSITION = { x: WIDTH / 3, y: HEIGHT / 2 };
 const GRAVITY = 0.2;
+const DEAD_ZONE_RANGE = 100;
+const DEAD_ZONE = {
+  minX: 0 - DEAD_ZONE_RANGE,
+  maxX: WIDTH + DEAD_ZONE_RANGE,
+  minY: 0 - DEAD_ZONE_RANGE,
+  maxY: HEIGHT + DEAD_ZONE_RANGE,
+};
 
 const SHARED_CONFIG = {
   width: WIDTH,
   height: HEIGHT,
   startPosition: START_POSITION,
+  deadZone: DEAD_ZONE,
 };
 
 const Scenes = [PreloadScene, PlayScene];
