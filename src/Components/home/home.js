1eeb865e0705cee0d186d26921d0561d4de917db
Jay_WooKJ <natcho9010@gmail.com>
2021-05-15 17:01:40 +0900

    Add: home component

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,8 +1,51 @@
 import html from "./home.html";
 import "./home.css";
+import backgroundImage from "../../../assets/images/ForestBG.png";
 
 function execute() {
+  const $home = document.getElementById("home");
+  const $nameInput = document.getElementById("name__input");
+  const $gameStartButton = document.getElementById("game__start");
+  const $rankingButton = document.getElementById("ranking__button");
+  const $scoreBoard = document.getElementById("score__board");
+
   console.log("home!!");
+  $home.style.backgroundImage = `url(${backgroundImage})`;
+
+  $gameStartButton.style.display = "none";
+  $scoreBoard.style.display = "none";
+
+  $nameInput.addEventListener("keyup", handleKeyupEvent);
+
+  $rankingButton.addEventListener("click", () => {
+    const scoreBoardDisplay = $scoreBoard.style.display;
+    console.log(scoreBoardDisplay);
+
+    if (scoreBoardDisplay === "none") {
+      $scoreBoard.style.display = "flex";
+    }
+  });
+
+  $scoreBoard.addEventListener("click", () => {
+    const scoreBoardDisplay = $scoreBoard.style.display;
+    console.log(scoreBoardDisplay);
+
+    if (scoreBoardDisplay === "flex") {
+      $scoreBoard.style.display = "none";
+    }
+  });
+
+  // api에서 받아오면 그걸 리스트해서 scoreBoard에 쭉 그려주면 된다.
+  function handleKeyupEvent(e) {
+    const inputName = e.target.value;
+    console.log(inputName);
+
+    if (inputName === "") {
+      $gameStartButton.style.display = "none";
+    } else {
+      $gameStartButton.style.display = "flex";
+    }
+  }
 }
 
 export default {
--- webpack.common.js
+++ webpack.common.js
@@ -44,6 +44,25 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   plugins: [
