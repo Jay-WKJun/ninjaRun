9198c1080daab16d682d8f42d5345b829ca93e02
Jay_WooKJ <natcho9010@gmail.com>
2021-05-20 15:02:56 +0900

    Add: player and score in resultScene

--- src/Game/scenes/modal/GameOverScene.js
+++ src/Game/scenes/modal/GameOverScene.js
@@ -16,9 +16,12 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   create() {
+    const player = this.game.player;
+    const score = this.registry.get("score");
+
     this.cameras.main.setBackgroundColor("rgba(0, 0, 0, 0.5)");
 
-    const deadCharacter = new Character(this, this.worldCenter.x, this.worldCenter.y, CHARACTER_DEAD, { isStatic: true });
+    const deadCharacter = new Character(this, this.worldCenter.x, this.worldCenter.y, CHARACTER_DEAD, { isStatic: true }).setScale(0.4);
 
     const animationKey1 = "stunAnimation";
     this.anims.create({
@@ -34,11 +37,57 @@ export default class GameOverScene extends Phaser.Scene {
       repeat: -1,
     });
 
-    this.stun = this.add.sprite(this.worldCenter.x, this.worldCenter.y - deadCharacter.displayHeight, STUN).setScale(0.2);
+    this.stun = this.add.sprite(this.worldCenter.x, this.worldCenter.y - deadCharacter.displayHeight, STUN).setScale(0.3);
     this.stun.play(animationKey1);
 
-    const restartButton = this.add.text(deadCharacter.x - deadCharacter.displayWidth, deadCharacter.y + deadCharacter.displayHeight, "Restart!").setOrigin(0.5).setInteractive();
-    const quitButton = this.add.text(deadCharacter.x + deadCharacter.displayWidth, deadCharacter.y + deadCharacter.displayHeight, "Quit!").setOrigin(0.5).setInteractive();
+    const restartButton = this.add.text(
+      deadCharacter.x - deadCharacter.displayWidth,
+      deadCharacter.y + deadCharacter.displayHeight,
+      "Restart?",
+      {
+        fontSize: "30px",
+      }
+    ).setOrigin(0.7, 0.5).setInteractive();
+    const quitButton = this.add.text(
+      restartButton.x + restartButton.displayWidth,
+      deadCharacter.y + deadCharacter.displayHeight,
+      "Save & Quit?",
+      {
+        fontSize: "30px",
+      }
+    ).setOrigin(0.2, 0.5).setInteractive();
+    this.playerNameLabel = this.add.text(
+      deadCharacter.x - 10,
+      deadCharacter.y + deadCharacter.displayHeight + 50,
+      "Player:",
+      {
+        fontSize: "20px",
+      }
+    ).setOrigin(1, 0.5);
+    this.playerNameText = this.add.text(
+      deadCharacter.x + 30,
+      deadCharacter.y + deadCharacter.displayHeight + 50,
+      player,
+      {
+        fontSize: "20px",
+      }
+    ).setOrigin(0, 0.5);
+    this.scoreLabel = this.add.text(
+      deadCharacter.x - 10,
+      deadCharacter.y + deadCharacter.displayHeight + 100,
+      "Score:",
+      {
+        fontSize: "20px",
+      }
+    ).setOrigin(1, 0.5);
+    this.scoreText = this.add.text(
+      deadCharacter.x + 30,
+      deadCharacter.y + deadCharacter.displayHeight + 100,
+      score,
+      {
+        fontSize: "20px",
+      }
+    ).setOrigin(0, 0.5);
 
     restartButton.on("pointerover", () => {
       restartButton.setStyle({ fill: "red" });
@@ -94,9 +143,6 @@ export default class GameOverScene extends Phaser.Scene {
     });
 
     quitButton.on("pointerup", () => {
-      const score = this.registry.get("score");
-      const player = this.game.player;
-
       this.game.saveRecord(player, score);
       this.game.router("", "/");
     });
