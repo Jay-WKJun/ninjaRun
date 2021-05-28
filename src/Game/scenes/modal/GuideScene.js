64a4d0d66b050b34a53b051a1abaa501137d3a05
Jay_WooKJ <natcho9010@gmail.com>
2021-05-28 17:21:31 +0900

    Correct: GuideScene

--- src/Game/scenes/modal/GuideScene.js
+++ src/Game/scenes/modal/GuideScene.js
@@ -25,6 +25,13 @@ export default class GuideScene extends Phaser.Scene {
     this.worldWidth = width;
     this.worldHeigth = height;
     this.worldCenter = { x: width / 2, y: height / 2 };
+    this.fontSize = "40px";
+
+    this.woodPanel = null;
+    this.woodTitlePanel = null;
+    this.woodRoundPanel = null;
+    this.redCross = null;
+    this.redCrossShadow = null;
   }
 
   preload() {
@@ -50,126 +57,211 @@ export default class GuideScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor("rgba(0, 0, 0, 0.5)");
 
-    this.woodPanel = this.add.image(this.worldCenter.x, this.worldCenter.y, WOOD_PANEL).setOrigin(0.5).setScale(0.6);
-    this.woodPanel.setDisplaySize(this.worldWidth * 0.9, this.worldHeigth * 0.9);
+    this.#createWoodPanel();
 
     const minX = this.worldCenter.x - (this.woodPanel.displayWidth / 2);
     const maxX = this.worldCenter.x + (this.woodPanel.displayWidth / 2);
     const minY = this.worldCenter.y - (this.woodPanel.displayHeight / 2);
 
-    this.woodTitlePanel = this.add.image(this.worldCenter.x, minY, WOOD_TITLE_PANEL).setOrigin(0.5, 0.3).setScale(0.2);
+    this.#createTitle(this.worldCenter.x, minY);
+    this.#createExitButton(maxX, minY);
 
-    this.woodRoundPanel = this.add.image(maxX, minY, WOOD_ROUND_PANEL).setOrigin(1, 0.3).setScale(0.1);
-
-    this.redCrossShadow = this.add.image(maxX - 15 + 5, minY + 5, RED_CROSS).setOrigin(1, 0.1).setScale(0.1).setVisible(false);
-    this.redCrossShadow.tint = 0x000000;
-    this.redCrossShadow.alpha = 0.6;
-
-    this.redCross = this.add.image(maxX - 15, minY, RED_CROSS).setOrigin(1, 0.1).setScale(0.1).setInteractive();
-
-    this.redCross.on("pointerover", () => {
-      this.redCrossShadow.setVisible(true);
-    });
-
-    this.redCross.on("pointerout", () => {
-      this.redCrossShadow.setVisible(false);
-    });
-
-    this.redCross.on("pointerup", () => {
-      window.localStorage.setItem("isVisit", true);
-
-      this.scene.remove();
-
-      this.parent.scene.add(this.nextScene.key, this.nextScene.object, true);
-      this.parent.scene.resume();
-    });
-
-    this.guideTitleText = this.add.text(this.worldCenter.x, minY, "How To Play!", {
-      fontSize: "40px",
-      fontStyle: "bold",
-    }).setOrigin(0.45, 0);
-
-    const fontSize = "40px";
     const textYOffset = 100;
     const textLineHeight = 130;
     const textIndenting = 70;
-    const whiteSpaceInterval = 30;
-
+    const whiteSpaceDistance = 30;
     const indentedXPosition = minX + textIndenting;
+
     const firstLineYCordinate = minY + textYOffset;
-
-    this.click = this.add.image(indentedXPosition, firstLineYCordinate, CLICK).setScale(0.2).setOrigin(0, 0.6);
-
-    const xPositionOfTextBeforeClick = indentedXPosition + this.click.displayWidth + whiteSpaceInterval;
-
-    this.textBeforeClick = this.add.text(xPositionOfTextBeforeClick, firstLineYCordinate, "to Shoot", {
-      fontSize,
-    });
-
-    const xPositionOfShurikent = xPositionOfTextBeforeClick + this.textBeforeClick.displayWidth + whiteSpaceInterval;
-
-    this.shuriken = this.add.image(xPositionOfShurikent, firstLineYCordinate, SHURIKEN).setOrigin(0, 0.3);
-
-    const xPositionOfTextBeforeShuriken = xPositionOfShurikent + this.shuriken.displayWidth + whiteSpaceInterval;
-
-    this.textBeforeShuriken = this.add.text(xPositionOfTextBeforeShuriken, firstLineYCordinate, "to", {
-      fontSize,
-    });
-
-    const xPositionOfMouseCursor = xPositionOfTextBeforeShuriken + this.textBeforeShuriken.displayWidth + whiteSpaceInterval;
-
-    this.mouseCursor = this.add.image(xPositionOfMouseCursor, firstLineYCordinate, MOUSE_CURSOR).setScale(0.1).setOrigin(0, 0.4);
+    this.#writeFirstLine(indentedXPosition, firstLineYCordinate, whiteSpaceDistance);
 
     const secondLineYCordinate = firstLineYCordinate + textLineHeight;
-
-    this.Akey = this.add.image(indentedXPosition, secondLineYCordinate, A_KEY).setScale(0.6).setOrigin(0, 0.3);
-
-    const xPositionOfDkey = indentedXPosition + this.Akey.displayWidth + whiteSpaceInterval;
-
-    this.Dkey = this.add.image(xPositionOfDkey, secondLineYCordinate, D_KEY).setScale(0.6).setOrigin(0, 0.3);
-
-    const xPositionOfTextBeforeDkey = xPositionOfDkey + this.Dkey.displayWidth + whiteSpaceInterval;
-
-    this.textBeforeDkey = this.add.text(xPositionOfTextBeforeDkey, secondLineYCordinate, "to Accelerate Swing!", {
-      fontSize,
-    });
+    this.#writeSecondLine(indentedXPosition, secondLineYCordinate, whiteSpaceDistance);
 
     const thirdLineYCordinate = secondLineYCordinate + textLineHeight;
-
-    this.thirdLineFirstText = this.add.text(indentedXPosition, thirdLineYCordinate, "Hit", {
-      fontSize,
-    });
-
-    const xPositionOfPlatform = indentedXPosition + this.thirdLineFirstText.displayWidth + whiteSpaceInterval;
-
-    this.platform = this.add.image(xPositionOfPlatform, thirdLineYCordinate, PLATFORM).setOrigin(0, 0.1).setScale(0.5);
-
-    const xPositionOfTextBeforePlatform = xPositionOfPlatform + this.platform.displayWidth + whiteSpaceInterval;
-
-    this.textBeforePlatform = this.add.text(xPositionOfTextBeforePlatform, thirdLineYCordinate, "to Swing!", {
-      fontSize,
-    });
+    this.#writeThirdLine(indentedXPosition, thirdLineYCordinate, whiteSpaceDistance);
 
     const forthLineYCordinate = thirdLineYCordinate + textLineHeight;
+    this.#writeForthLine(indentedXPosition, forthLineYCordinate, whiteSpaceDistance);
+  }
 
-    this.forthLineFirstText = this.add.text(indentedXPosition, forthLineYCordinate, "Hit", {
-      fontSize,
-    });
+  #createWoodPanel() {
+    this.woodPanel = this.add.image(this.worldCenter.x, this.worldCenter.y, WOOD_PANEL)
+      .setOrigin(0.5)
+      .setScale(0.6);
+    this.woodPanel.setDisplaySize(this.worldWidth * 0.9, this.worldHeigth * 0.9);
+  }
 
-    const xPositionOfRedBird = indentedXPosition + this.forthLineFirstText.displayWidth + whiteSpaceInterval;
+  #createTitle(xCordinate, yCordinate) {
+    createWoodTitlePanel(xCordinate, yCordinate);
+    createTitleText(xCordinate, yCordinate, "How To Play!");
 
-    this.redBird = this.add.image(xPositionOfRedBird, forthLineYCordinate, RED_BIRD).setOrigin(0, 0.2).setScale(0.1);
+    function createWoodTitlePanel(xCordinate, yCordinate) {
+      this.woodTitlePanel = this.add.image(xCordinate, yCordinate, WOOD_TITLE_PANEL)
+        .setOrigin(0.5, 0.3)
+        .setScale(0.2);
+    }
 
-    const xPositionOfTextBeforeRedBird = xPositionOfRedBird + this.redBird.displayWidth + whiteSpaceInterval;
+    function createTitleText(xCordinate, yCordinate, textContent) {
+      this.guideTitleText = this.add.text(xCordinate, yCordinate, textContent, {
+        fontSize: this.fontSize,
+        fontStyle: "bold",
+      }).setOrigin(0.45, 0);
+    }
+  }
 
-    this.textBeforeRedBird = this.add.text(xPositionOfTextBeforeRedBird, forthLineYCordinate, "or Each 10 Sec, Score", {
-      fontSize,
-    });
+  #createExitButton(xCordinate, yCordinate) {
+    createWoodRoundPanel(xCordinate, yCordinate);
+    createRedCross(xCordinate, yCordinate);
 
-    const xPositionOfPlusOne = xPositionOfTextBeforeRedBird + this.textBeforeRedBird.displayWidth + whiteSpaceInterval;
+    function createWoodRoundPanel(xCordinate, yCordinate) {
+      this.woodRoundPanel = this.add.image(xCordinate, yCordinate, WOOD_ROUND_PANEL)
+        .setOrigin(1, 0.3)
+        .setScale(0.1);
+    }
 
-    this.whiteCircle = this.add.image(xPositionOfPlusOne - 3, forthLineYCordinate + 5, WHITE_CIRCLE).setOrigin(0.2, 0.4).setScale(0.3);
+    function createRedCross(xCordinate, yCordinate) {
+      this.redCross = this.add.image(xCordinate - 15, yCordinate, RED_CROSS)
+        .setOrigin(1, 0.1)
+        .setScale(0.1)
+        .setInteractive();
 
-    this.plusOne = this.add.image(xPositionOfPlusOne, forthLineYCordinate, PLUS_ONE).setOrigin(0, 0.4).setScale(0.2);
+      this.redCrossShadow = this.add.image(xCordinate - 15 + 5, yCordinate + 5, RED_CROSS)
+        .setOrigin(1, 0.1)
+        .setScale(0.1)
+        .setVisible(false);
+      this.redCrossShadow.tint = 0x000000;
+      this.redCrossShadow.alpha = 0.6;
+
+      this.redCross.on("pointerover", () => {
+        this.redCrossShadow.setVisible(true);
+      });
+
+      this.redCross.on("pointerout", () => {
+        this.redCrossShadow.setVisible(false);
+      });
+
+      this.redCross.on("pointerup", () => {
+        window.localStorage.setItem("isVisit", true);
+
+        this.scene.remove();
+
+        this.parent.scene.add(this.nextScene.key, this.nextScene.object, true);
+        this.parent.scene.resume();
+      });
+    }
+  }
+
+  #writeFirstLine(firstXCoord, yCoord, wSpace) {
+    this.click = this.add.image(firstXCoord, yCoord, CLICK)
+      .setOrigin(0, 0.6)
+      .setScale(0.2);
+
+    const secondXCoord = this.#getNextXCordinate(firstXCoord, this.click, wSpace);
+    this.textBeforeClick = this.#createNormalText(
+      secondXCoord,
+      yCoord,
+      "to Shoot"
+    );
+
+    const thirdXCoord = this.#getNextXCordinate(secondXCoord, this.textBeforeClick, wSpace);
+    this.shuriken = this.add.image(thirdXCoord, yCoord, SHURIKEN)
+      .setOrigin(0, 0.3);
+
+    const forthXCoord = this.#getNextXCordinate(thirdXCoord, this.shuriken, wSpace);
+    this.textBeforeShuriken = this.#createNormalText(
+      forthXCoord,
+      yCoord,
+      "to"
+    );
+
+    const fifthXCoord = this.#getNextXCordinate(forthXCoord, this.textBeforeShuriken, wSpace);
+    this.mouseCursor = this.add.image(fifthXCoord, yCoord, MOUSE_CURSOR)
+      .setOrigin(0, 0.4)
+      .setScale(0.1);
+  }
+
+  #writeSecondLine(firstXCoord, yCoord, wSpace) {
+    this.Akey = this.add.image(firstXCoord, yCoord, A_KEY)
+      .setOrigin(0, 0.3)
+      .setScale(0.6);
+
+    const secondXCoord = this.#getNextXCordinate(firstXCoord, this.Akey, wSpace);
+    this.Dkey = this.add.image(secondXCoord, yCoord, D_KEY)
+      .setOrigin(0, 0.3)
+      .setScale(0.6);
+
+    const thirdXCoord = this.#getNextXCordinate(secondXCoord, this.Dkey, wSpace);
+    this.textBeforeDkey = this.#createNormalText(
+      thirdXCoord,
+      yCoord,
+      "to Accelerate Swing!"
+    );
+  }
+
+  #writeThirdLine(firstXCoord, yCoord, wSpace) {
+    this.thirdLineFirstText = this.#createNormalText(
+      firstXCoord,
+      yCoord,
+      "Hit"
+    );
+
+    const secondXCoord = this.#getNextXCordinate(firstXCoord, this.thirdLineFirstText, wSpace);
+    this.platform = this.add.image(secondXCoord, yCoord, PLATFORM)
+      .setOrigin(0, 0.1)
+      .setScale(0.5);
+
+    const thirdXCoord = this.#getNextXCordinate(secondXCoord, this.platform, wSpace);
+    this.textBeforePlatform = this.#createNormalText(
+      thirdXCoord,
+      yCoord,
+      "to Swing!"
+    );
+  }
+
+  #writeForthLine(firstXCoord, yCoord, wSpace) {
+    this.forthLineFirstText = this.#createNormalText(
+      firstXCoord,
+      yCoord,
+      "Hit"
+    );
+
+    const secondXCoord = this.#getNextXCordinate(firstXCoord, this.forthLineFirstText, wSpace);
+    this.redBird = this.add.image(secondXCoord, yCoord, RED_BIRD)
+      .setOrigin(0, 0.2)
+      .setScale(0.1);
+
+    const thirdXCoord = this.#getNextXCordinate(secondXCoord, this.redBird, wSpace);
+    this.textBeforeRedBird = this.#createNormalText(
+      thirdXCoord,
+      yCoord,
+      "or Each 10 Sec, Score"
+    );
+
+    const forthXCoord = this.#getNextXCordinate(thirdXCoord, this.textBeforeRedBird, wSpace);
+    this.whiteCircle = this.add.image(forthXCoord - 3, yCoord + 5, WHITE_CIRCLE)
+      .setOrigin(0.2, 0.4)
+      .setScale(0.3);
+
+    this.plusOne = this.add.image(forthXCoord, yCoord, PLUS_ONE)
+      .setOrigin(0, 0.4)
+      .setScale(0.2);
+  }
+
+  #createNormalText(xCoordinate, yCoordinate, textContent) {
+    const textObject = this.add.text(
+      xCoordinate,
+      yCoordinate,
+      textContent,
+      {
+        fontSize: this.fontSize,
+      }
+    );
+
+    return textObject;
+  }
+
+  #getNextXCordinate(previousXCoordinate, frontObject, whiteSpaceDistance) {
+    return previousXCoordinate + frontObject.displayWidth + whiteSpaceDistance;
   }
 }
